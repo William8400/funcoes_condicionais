6d2879a2f04e86b69e26707846bfd9d7735fa04d
William8400 <wdonato02@gmail.com>
2025-09-22 15:37:09 -0300

    nova função

--- pedido_folha.php
+++ pedido_folha.php
@@ -1,8 +1,53 @@
 <?php
 
 
+function qual_seu_cla($nome_ninja, $numero_ninja)
+{
+
+    echo "Ninja: " . $nome_ninja . "<br>";
+
+    if ($numero_ninja == 1) {
+
+        echo "Ninja do clã Uchiha! ";
+        descricao_cla($numero_ninja);
+
+    } else if ($numero_ninja == 2) {
+
+        echo "Ninja do clã Hyuuga! ";
+        descricao_cla($numero_ninja);
+
+    } else if ($numero_ninja == 3) {
+
+        echo "Ninja do clã Nara!";
+        descricao_cla($numero_ninja);
+
+    } else if ($numero_ninja == 4) {
+        
+        echo "Ninja do clã Aburame!";
+        descricao_cla($numero_ninja);
+
+    } else {
+
+        echo  "Número do ninja inválido";
+    }
+}
+
+
+function descricao_cla($numero_ninja)
+{
+    if ($numero_ninja == 1) {
+        echo  "Um texto qualquer.";
+    } else if ($numero_ninja == 2) {
+        echo  "Um texto qualquer";
+    } else if ($numero_ninja == 3) {
+        echo  "Um texto qualquer";
+    } else if ($numero_ninja == 4) {
+        echo  "Um texto qualquer";
+    } else if ($numero_ninja) {
+        echo  "Um texto qualquer";
+    }
+}
 
 
 
-
-?>
+qual_seu_cla('Hatake', 1);
